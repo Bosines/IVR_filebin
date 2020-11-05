734f1984279d01a12f1924c93265a1de365915ce
Bosines <mister.ti199@gmail.com>
2020-11-05 15:35:29 +0300

    All buttons in slides added

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -344,6 +344,7 @@ public class MenuScript : MonoBehaviour
             way.SetActive(true);
             WayVisible = true;
             ShowChosen(false);
+            SliceButtVisible = true;
         }
         
         
